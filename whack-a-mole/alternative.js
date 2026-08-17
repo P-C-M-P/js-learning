89b6ddc9163fbee54f4541463c601bbe82247d20
P-C-M-P <soragabelti@gmail.com>
2026-08-17 20:20:04 +0000

    ADD some code i guess

--- whack-a-mole/alternative.js
+++ whack-a-mole/alternative.js
@@ -1,5 +1,11 @@
 const start = document.getElementById("start");
 let squares = document.querySelectorAll(".square")
+const controls = document.querySelector("#controls")
+const Pause = document.querySelector("#pause")
+const Resume = document.querySelector("#resume")
+const Restart = document.querySelector("#reStart")
+const topBar = document.getElementById("topBar")
+
 
 let mole = document.createElement("img"); 
 mole.src="images/mole.png"
@@ -8,8 +14,10 @@ let score = 0;
 let currentResult = document.getElementById("score")
 let currentTime = document.getElementById("time-left");
 let time = 60
+let storedTime;
 let gameDuration;
 let previousSquare = null;
+let isPaused = false;
 
 function moveMole(){
     let randomPosition = Math.floor(Math.random() * 9) + 1
@@ -21,6 +29,8 @@ function moveMole(){
         square.onclick = function(){
             if(square === currentSquare){
                 score++;
+                mole.remove()
+                currentResult.onclick = null;
             }   
         };
     });
@@ -40,20 +50,47 @@ function moveMole(){
 }
 
 function countDown(){
-        moveMole();
-        currentTime.textContent = time;
-        time--;
-        currentResult.textContent = score;
+        
 
-
-    if(time === 0){
-        clearInterval(gameDuration)
+    if(time === -1){
         alert(`GAME OVER! your score is ${score}`)
-        time = 60;
-        score = 0;
+        clearInterval(gameDuration)
+        // time = 60;
+        // score = 0;
+    }
+    else{
+        moveMole();
+        currentTime.textContent = "Time-left: " + time;
+       // storedTime = time;
+        currentResult.textContent = "Score: " + score;
+        time--;
     }
 }
 
 start.addEventListener("click", () => {
+    start.style.display = "none";
+    controls.style.display = "inline-block";
+    topBar.style.display = "flex"
+    gameDuration = setInterval(countDown, 1000)
+})
+
+Pause.addEventListener("click", () => {
+    clearInterval(gameDuration)
+    isPaused = true
+})
+
+Resume.addEventListener("click", () => {
+    if(isPaused){
+        gameDuration = setInterval(countDown, 1000)
+        isPaused = false
+    }
+})
+
+Restart.addEventListener("click", () => {
+    clearInterval(gameDuration)
+    time = 60;
+    score = 0;
+    currentTime.textContent = time;
+    currentResult.textContent = score;
     gameDuration = setInterval(countDown, 1000)
 })
